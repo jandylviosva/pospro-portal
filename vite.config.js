285466ce7022db0cb38d8869c8b9d6280e1b8298
Jan <jandylviosva@gmail.com>
2026-07-07 22:04:12 +0800

    Add billPayment entry to Rollup input options

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,7 @@ export default defineConfig({
       input: {
         main: resolve(__dirname, 'index.html'),
         payment: resolve(__dirname, 'payment.html'),
+        billPayment: resolve(__dirname, 'bill-payment.html'),
       },
     },
   },
