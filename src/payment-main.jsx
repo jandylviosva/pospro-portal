15ace563dec027ac86fd4148d83c5da04e43adb1
Jan <jandylviosva@gmail.com>
2026-07-07 19:01:56 +0800

    Add PaymentApp component to payment-main.jsx

---
+++ src/payment-main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import PaymentApp from './PaymentApp.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <PaymentApp />
+  </React.StrictMode>
+)
