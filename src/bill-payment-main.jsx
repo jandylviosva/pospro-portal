f2aaaa12b061146099e6cda37c71469be3368096
Jan <jandylviosva@gmail.com>
2026-07-07 22:05:54 +0800

    Create bill-payment-main.jsx

---
+++ src/bill-payment-main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import BillPaymentApp from './BillPaymentApp.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BillPaymentApp />
+  </React.StrictMode>
+)
