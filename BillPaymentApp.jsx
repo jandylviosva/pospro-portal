4e5d01c65e1501155c8adc1ac48178e5c14b6528
Jan <jandylviosva@gmail.com>
2026-07-17 12:39:58 +0800

    Delete BillPaymentApp.jsx

--- BillPaymentApp.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import BookingPayApp from './BookingPayApp.jsx'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <BookingPayApp />
-  </React.StrictMode>
-)
